4c7ad322d8f98cb4f9399d0fc34d39320d18819f
Adrian Eguez <eadepto@hotmail.com>
2023-07-10 09:14:02 -0500

    hoy

--- 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/ECrudEntrenador.kt
+++ 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/ECrudEntrenador.kt
@@ -2,10 +2,29 @@ package com.example.movilescomputacion2023a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ECrudEntrenador : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecrud_entrenador)
+
+        val botonBuscarBDD = findViewById<Button>(R.id.btn_buscar_bdd)
+        botonBuscarBDD
+            .setOnClickListener {
+                val id = findViewById<EditText>(R.id.input_id)
+                val nombre = findViewById<EditText>(R.id.input_nombre)
+                val descripcion = findViewById<EditText>(
+                    R.id.input_descripcion
+                )
+                val entrenador = EBaseDeDatos.tablaEntrenador!!
+                    .consultarEntrenadorPorID(
+                        id.text.toString().toInt()
+                    )
+                id.setText(entrenador.id.toString())
+                nombre.setText(entrenador.nombre)
+                descripcion.setText(entrenador.descripcion)
+            }
     }
 }
