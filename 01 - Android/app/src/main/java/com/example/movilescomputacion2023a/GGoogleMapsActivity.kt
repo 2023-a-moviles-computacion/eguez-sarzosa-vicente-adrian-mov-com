68e96821ed1a8c21efe640e2d929ea7f868839cb
Adrian Eguez <eadepto@hotmail.com>
2023-07-25 10:07:16 -0500

    hasta mapas

--- 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/GGoogleMapsActivity.kt
+++ 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/GGoogleMapsActivity.kt
@@ -3,6 +3,8 @@ package com.example.movilescomputacion2023a
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -19,7 +21,20 @@ class GGoogleMapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ggoogle_maps)
         solicitarPermisos()
         iniciarLogicaMapa()
+        val boton = findViewById<Button>(R.id.btn_ir_carolina)
+        boton.
+        setOnClickListener {
+            irCarolina()
+        }
     }
+
+    fun irCarolina(){
+        val carolina = LatLng(-0.1825684318486696,
+            -78.48447277600916)
+        val zoom = 17f
+        moverCamaraConZoom(carolina, zoom)
+    }
+
     fun solicitarPermisos(){
         val contexto = this.applicationContext
         val nombrePermiso = android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -55,7 +70,32 @@ class GGoogleMapsActivity : AppCompatActivity() {
                 moverQuicentro()
                 anadirPolilinea()
                 anadirPoligono()
+                escucharListeners()
             }
+        }
+    }
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnPolylineClickListener {
+            Log.i("mapa", "setOnPolylineClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnMarkerClickListener {
+            Log.i("mapa", "setOnMarkerClickListener ${it}")
+            it.tag // ID
+            return@setOnMarkerClickListener true
+        }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener")
+        }
+        mapa.setOnCameraMoveStartedListener {
+            Log.i("mapa", "setOnCameraMoveStartedListener ${it}")
+        }
+        mapa.setOnCameraIdleListener {
+            Log.i("mapa", "setOnCameraIdleListener")
         }
     }
 
@@ -109,6 +149,8 @@ class GGoogleMapsActivity : AppCompatActivity() {
         markQuicentro.tag = titulo
         moverCamaraConZoom(quicentro, zoom)
     }
+
+
 
 
 
