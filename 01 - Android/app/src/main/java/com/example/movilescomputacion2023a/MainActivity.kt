569a6051c794af1dec9cf7908bac2840fdb7026a
Adrian Eguez <eadepto@hotmail.com>
2023-08-15 08:56:53 -0500

    hasta botones

--- 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/MainActivity.kt
+++ 01 - Android/app/src/main/java/com/example/movilescomputacion2023a/MainActivity.kt
@@ -108,6 +108,13 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(HFirebaseUIAuth::class.java)
             }
+
+
+        val botonFirestore = findViewById<Button>(R.id.btn_intent_firestore)
+        botonFirestore
+            .setOnClickListener {
+                irActividad(IFirestore::class.java)
+            }
     }
     fun abrirActividadConParametros(
         clase: Class<*>
